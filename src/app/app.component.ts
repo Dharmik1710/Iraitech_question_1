fbed5a8ec743704616c57f5708ae4a1c6a4b2609
Dharmik Patel <dharmik1289@gmail.com>
2020-11-25 11:53:34 +0530

    question 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ques1';
+  index: number;
+  array = [2,3,10,15,26,35];
+  result: number;
+  remTime: number = 5;
+  
+  onSubmit(){
+    if(this.index==undefined){
+      return;
+    }
+    while(this.index>=this.array.length){
+      this.array.push(8+4*(this.array.length-2) + this.array[this.array.length-2]);
+    }
+    this.result = this.array[this.index];
+    
+    // Reset after 5 seconds
+    let interval = setInterval(() => {
+      this.remTime--;
+    }, 1000)
+    
+    setTimeout(() => {
+      clearInterval(interval);
+      this.index = undefined;
+      this.result = undefined;
+      this.remTime = 5;
+    }, 5000);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
